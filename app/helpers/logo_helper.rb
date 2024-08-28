024c7d2135337e2261d749e6a612f4f5bb96675a
Chris Oliver <excid3@gmail.com>
2024-08-28 16:10:01 -0500

    Fix helper

--- app/helpers/logo_helper.rb
+++ app/helpers/logo_helper.rb
@@ -1,7 +1,7 @@
 module LogoHelper
   def logo(class: "h-6")
     if SellRepo.config.logo.attached?
-      image_tag(SellRepo.config.logo, class: "h-6", alt: store_name)
+      image_tag(SellRepo.config.logo, class: "h-6", alt: SellRepo.store_name)
     else
       SellRepo.store_name
     end
