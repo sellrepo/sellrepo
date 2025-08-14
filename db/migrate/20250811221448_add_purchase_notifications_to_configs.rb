1ac18ef8bc4ae0459780496ae42ef4d4ef9444c9
Chris Oliver <excid3@gmail.com>
2025-08-14 09:00:04 -0500

    Rename migration

--- db/migrate/20250811221448_add_purchase_notifications_to_configs.rb
+++ db/migrate/20250811221448_add_purchase_notifications_to_configs.rb
@@ -1,4 +1,4 @@
-class AddPurchaseNotificationsEnabledToConfigs < ActiveRecord::Migration[8.0]
+class AddPurchaseNotificationsToConfigs < ActiveRecord::Migration[8.0]
   def change
     add_column :configs, :purchase_notifications, :boolean, default: true
   end
